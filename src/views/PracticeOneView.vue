501d4d3e1dcd69cef2345548a08709fe9abbdcb3
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-25 21:05:56 +0800

    Update PracticeOneView.vue

--- src/views/PracticeOneView.vue
+++ src/views/PracticeOneView.vue
@@ -5,7 +5,27 @@ export default {
   data() {
     return {
       vueLogo,
+      msg: '',
+      selectedTab: 'logo',
     };
+  },
+  methods: {
+    changeTab(tab, msg = '') {
+      this.selectedTab = tab;
+      this.msg = msg;
+    },
+    bgColor() {
+      if (this.selectedTab === 'number') {
+        return 'bg-red-500';
+      }
+      if (this.selectedTab === 'name') {
+        return 'bg-blue-500';
+      }
+      if (this.selectedTab === 'food') {
+        return 'bg-green-500';
+      }
+      return '';
+    },
   },
 };
 </script>
@@ -13,13 +33,14 @@ export default {
 <template>
   <div class="w-[700px] h-[50dvh]">
     <nav class="flex">
-      <button type="button" class="nav-btn active">Vue</button>
-      <button type="button" class="nav-btn">編號</button>
-      <button type="button" class="nav-btn">姓名</button>
-      <button type="button" class="nav-btn">喜歡的食物</button>
+      <button type="button" class="nav-btn" :class="{ 'active': selectedTab === 'logo' }" @click="changeTab('logo')">Vue</button>
+      <button type="button" class="nav-btn" :class="{ 'active': selectedTab === 'number' }" @click="changeTab('number', '0')">編號</button>
+      <button type="button" class="nav-btn" :class="{ 'active': selectedTab === 'name' }" @click="changeTab('name', 'Stanley')">姓名</button>
+      <button type="button" class="nav-btn" :class="{ 'active': selectedTab === 'food' }" @click="changeTab('food', '拉麵')">喜歡的食物</button>
     </nav>
-    <div class="flex items-center justify-center h-full border-2 border-white rounded-b rounded-tr">
-      <img :src="vueLogo" alt="Vue Logo" class="w-1/2 h-1/2">
+    <div class="flex items-center justify-center h-full border-2 border-white rounded-b rounded-tr" :class="{ 'bg-red-500': selectedTab === 'number', 'bg-blue-500': selectedTab === 'name', 'bg-green-500': selectedTab === 'food' }">
+      <img v-show="selectedTab === 'logo'" :src="vueLogo" alt="Vue Logo" class="w-1/2 h-1/2">
+      <div v-show="selectedTab !== 'logo'" class="text-8xl">{{ msg }}</div>
     </div>
   </div>
 </template>
