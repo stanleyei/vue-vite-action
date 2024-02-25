b476629a3d33492b33828345b14c49eaf0fd2708
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2024-02-25 18:58:31 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-vite-action/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
   server: {
     open: true,
   },
-})
+});
